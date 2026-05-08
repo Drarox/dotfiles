b4c8ce59e55415cd96a23e612972ec1b9e03e0e7
Yannick <yannick.b443@gmail.com>
2026-05-08 12:58:52 +0200

    Nginx config updated

--- server/nginx-reverse-setup.sh
+++ server/nginx-reverse-setup.sh
@@ -15,32 +15,37 @@ curl -s https://raw.githubusercontent.com/Drarox/dotfiles/main/server/nginx-conf
 read -p "Press [Enter] to continue when done ..."
 
 read -p "Press [Enter] to configure /var/lib/docker/volumes/reverse_nginx_reverse_conf/_data ..."
-cd /var/lib/docker/volumes/reverse_nginx_reverse_conf/_data
+cd /var/lib/docker/volumes/reverse_nginx_reverse_conf/_data || { echo "Error: Volume not found!" >&2; exit 1; }
 mkdir sites-available
 cd sites-available
 curl -O https://raw.githubusercontent.com/Drarox/dotfiles/main/server/nginx-config/example
 cd ..
 mkdir sites-enabled
-openssl dhparam -out dhparam.pem 2048
 curl -O https://raw.githubusercontent.com/Drarox/dotfiles/main/server/nginx-config/cloudflare
 curl -O https://raw.githubusercontent.com/Drarox/dotfiles/main/server/nginx-config/nginx.conf
-cd conf.d
-curl -O https://raw.githubusercontent.com/Drarox/dotfiles/main/server/nginx-config/default.conf
+cd conf.d # Default
+curl -O https://raw.githubusercontent.com/Drarox/dotfiles/main/server/nginx-config/00‑catchall.conf
+cd ..
+mkdir ssl # Create self signed certificates for 00‑catchall.conf
+cd ssl
+openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout default.key -out default.crt
+cd ..
+mkdir snippets
+cd snippets
+curl -O https://raw.githubusercontent.com/Drarox/dotfiles/main/server/nginx-config/snippets/letsencrypt
+curl -O https://raw.githubusercontent.com/Drarox/dotfiles/main/server/nginx-config/snippets/proxy-params.conf
+curl -O https://raw.githubusercontent.com/Drarox/dotfiles/main/server/nginx-config/snippets/websocket.conf
 cd ..
 
 read -p "Press [Enter] to install and configure cerbot ..."
 apt install certbot -y
-mkdir snippets
-cd snippets
-curl -O https://raw.githubusercontent.com/Drarox/dotfiles/main/server/nginx-config/letsencrypt
-cd ..
 #Add crontab to renew certificates
 (crontab -l 2>/dev/null; echo "30 1 * * 1 /usr/bin/certbot renew >> /var/log/le-renew.log") | crontab -
 # Certbot hook to reload nginx automatically:
 echo 'deploy-hook = docker exec Nginx_Reverse /usr/sbin/nginx -s reload' | sudo tee -a /etc/letsencrypt/cli.ini
 
 echo "To create a certificate, run the following command:"
-echo "sudo certbot certonly --webroot -w /var/www/letsencrypt --agree-tos --no-eff-email --email name@domain.com -d domain.com --rsa-key-size 4096"
+echo "sudo certbot certonly --webroot -w /var/www/letsencrypt --agree-tos --no-eff-email --email name@domain.com -d domain.com"
 
 read -p "Press [Enter] to restart the nginx container and finish ..."
 docker restart Nginx_Reverse
